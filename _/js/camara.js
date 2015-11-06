0cae470974643b1572cb4522e27190ea54acc9d1
Amparo Megias Castillo <amparo@MacBook-Pro-de-Amparo.local>
2015-11-06 22:30:11 +0100

    fin camara

--- _/js/camara.js
+++ _/js/camara.js
@@ -13,9 +13,11 @@ var miapp = {
 	micontexto:"",
 	
 	iniciar: function(){
+		
 		//En la variable miVideo se almacena una referencia al elemento con id media
 		this.miVideo = document.getElementById('media');
-		//Le asigno alto y ancho de la ventana
+		
+		//Al elemento con id media le asigno alto y ancho de la ventana
 		this.miVideo.width = window.innerWidth;
 		this.miVideo.height = window.innerHeight;
 		
@@ -23,16 +25,28 @@ var miapp = {
 		this.miFoto = document.getElementById('foto');
 		
 		//En la variable micanvas se almacena una referencia al elemento con id canvas
-		miapp.micanvas = document.getElementById('canvas');
+		this.micanvas = document.getElementById('canvas');
 		
 		// Almaceno la referencia al elemento type button con id grabar del formulario y le registro un detector para el evento 'onclick'*/
 		var boton=document.getElementById('grabar');
 		boton.addEventListener('click', this.nuevoitem);
 		
+		//El evento hide.bs.modal se lanza cuando la ventana(en este caso de alta) se cierra.
+		$('#myModal').on('hidden.bs.modal',miapp.resetformalta);
+		
 		//Aseguro la compatibilidad de la API en los distintos navegadores
 		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 		//Método para tener acceso a la cámara web. En caso de éxito se ejecuta la función exito()
 		navigator.getUserMedia({video:true}, miapp.exito, miapp.mostraerror);		
+	},
+	
+	//Esta función se ejecuta cuando se cierra la ventana modal que contiene el formulario de altas
+	resetformalta: function(){
+		/*
+		*Limpio todos los campos del formulario de alta.
+		*/ 
+		document.getElementById('titulo').value="";
+		document.getElementById('descripcion').value="";
 	},
 	
 	//Esta función se ejecuta si el usuario accede a que la app tenga acceso a la cámara web.
@@ -43,14 +57,15 @@ var miapp = {
 		
 		//Usando el método createObjectURL() se obtiene la URL que representa el stream
 		//La URL se asigna al atributo src de elemento <video>
-		this.miVideo.setAttribute('src', URL.createObjectURL(stream));
+		miapp.miVideo.setAttribute('src', URL.createObjectURL(stream));
 		
 		//El vídeo se reproduce
-		this.miVideo.play();
+		miapp.miVideo.play();
 		
-		//Se añade un detector para el evento onclick en el elemento de tipo button con id foto
-		this.miFoto.addEventListener('click', miapp.instantanea.bind(this.miVideo));
-			
+		//En la variable miFoto se almacena una referencia al elemento con id foto
+		miapp.miFoto = document.getElementById('foto');
+		//Se añade un detector para el evento onclick en el elemento <video>
+		miapp.miFoto.addEventListener('click', miapp.instantanea.bind(miapp.miVideo));			
 	},
 	
 	mostraerror: function(e){
@@ -68,9 +83,7 @@ var miapp = {
 		miapp.micontexto.drawImage(this, 0, 0, 320, 240);
 
 		//Método que devuelve los datos de la imagen
-		miapp.dataURL = miapp.micanvas.toDataURL();
-		
-	
+		miapp.dataURL = miapp.micanvas.toDataURL();			
 	},
 	
 	nuevoitem: function(){
@@ -91,8 +104,7 @@ var miapp = {
 		var img_datos = {
 		    titulo: titulo,
 		    descripcion: descripcion,
-		    imagen: miapp.dataURL
-		
+		    imagen: miapp.dataURL		
 		};
 		
 		/*
@@ -104,8 +116,7 @@ var miapp = {
 		/*
 		*Limpio todos los campos del formulario de alta.
 		*/ 
-		document.getElementById('titulo').value="";
-		document.getElementById('descripcion').value="";
+		miapp.resetformalta();
 		miapp.micontexto.clearRect(0,0,miapp.micanvas.width, miapp.micanvas.height);
 		
 		//Cierro la ventana modal donde se encuentra el formulario
